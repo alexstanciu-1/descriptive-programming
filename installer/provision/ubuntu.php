013c6e481dd01c8f5c85fa6841068cfdd18986fc
Alex Stanciu <ealexs@gmail.com>
2025-12-08 09:22:11 +0200

    update

--- installer/provision/ubuntu.php
+++ installer/provision/ubuntu.php
@@ -3,5 +3,8 @@
 # install crontab start crontab
 # start services when ubuntu starts !!!!
 
-
 echo "provision/ubuntu\n";
+
+shell_exec("apt update");
+shell_exec("apt install php-fpm apache2 mariadb-server phpmyadmin");
+
