4f7b74a0d139c95b25787d3d94985aed38aedde6
Alex Stanciu <ealexs@gmail.com>
2025-12-09 09:45:38 +0200

    update

--- installer/provision/ubuntu.php
+++ installer/provision/ubuntu.php
@@ -4,7 +4,10 @@
 # start services when ubuntu starts !!!!
 
 echo "provision/ubuntu\n";
-var_dump($argv);
+
+$provision_for_user = trim($argv[1] ?? '');
+
+var_dump('$provision_for_user: ' . $provision_for_user . "\n");
 echo "\n\n";
 
 s_exec("DEBIAN_FRONTEND=noninteractive apt install -y");
