ad64a2bba10d8385154baae60a0aa9c3ef595b6c
Alex Stanciu <ealexs@gmail.com>
2025-12-09 09:44:35 +0200

    update

--- installer/provision/ubuntu.php
+++ installer/provision/ubuntu.php
@@ -4,12 +4,18 @@
 # start services when ubuntu starts !!!!
 
 echo "provision/ubuntu\n";
+var_dump($argv);
+echo "\n\n";
 
 s_exec("DEBIAN_FRONTEND=noninteractive apt install -y");
 s_exec("DEBIAN_FRONTEND=noninteractive apt install -f");
 s_exec("DEBIAN_FRONTEND=noninteractive apt update -y");
-s_exec("DEBIAN_FRONTEND=noninteractive apt install -y php-fpm apache2 mariadb-server phpmyadmin");
+s_exec("DEBIAN_FRONTEND=noninteractive apt install -y php-fpm apache2 mariadb-server phpmyadmin cron");
 
+# systemctl start cron
+# /etc/wsl.conf
+# [boot]
+# command="service ssh start; service cron start"
 
 function s_exec(string $command)
 {
