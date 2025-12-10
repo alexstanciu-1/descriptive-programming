f81b22c3ff8d7681348523790bfe87e2fdb3e96b
Alex Stanciu <ealexs@gmail.com>
2025-12-10 09:55:26 +0200

    update

---
+++ installer/windows_wsl/install.cmd
@@ -0,0 +1,38 @@
+:: !!! WE ASUME %username% is the linux user to be used
+:: test command: git commit -am update; git push; "./installer/windows_wsl/install.cmd"
+
+:: remove current ubuntu
+:: wslconfig /u Ubuntu
+
+:: make sure windows subsystem for linux is installed !!!
+wsl --install -d Ubuntu
+
+:: run a update if needed
+:: wsl --update
+
+wsl -u root -d Ubuntu -- apt update
+wsl -u root -d Ubuntu -- apt install -y git php
+
+wsl -d Ubuntu -- git clone https://github.com/alexstanciu-1/descriptive-programming.git ~/descriptive-app
+
+:: make sure we update, in case it's not the first run
+wsl -d Ubuntu -- git -C ~/descriptive-app pull
+
+:: This is the main provisioner. We need root access.
+wsl -u root -d Ubuntu -- php /home/%username%/descriptive-app/installer/provision/ubuntu.php %username%
+
+start http://localhost:8080
+
+pause
+
+:: SOME COMMANDS TO KEEP IN MIND
+:: go to current dir
+:: cd /D "%~dp0"
+:: echo Now about to end...
+:: pause
+:: set /p DUMMY=Hit ENTER to continue...
+:: wsl whoami
+:: ubuntu -c "echo $USER"
+:: wsl -u root -d Ubuntu -- echo "I am $USER"
+:: wsl -d Ubuntu -- echo "I am $USER"
+:: wsl -d Ubuntu -- git --version
