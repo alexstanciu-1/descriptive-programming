093409a11fb1ee0d4c671a1db1eead90e057fee8
Alex Stanciu <ealexs@gmail.com>
2025-12-20 11:46:52 +0200

    update

--- installer/windows_wsl/install.cmd
+++ installer/windows_wsl/install.cmd
@@ -28,14 +28,14 @@ wsl --install -d Ubuntu
 wsl -u root -d Ubuntu -- apt update
 wsl -u root -d Ubuntu -- apt install -y git php
 
-wsl -u root -d Ubuntu -- git clone https://github.com/alexstanciu-1/descriptive-programming.git /usr/share/descriptive-app
+wsl -u root -d Ubuntu -- git clone https://github.com/alexstanciu-1/descriptive-programming.git /home/descriptive-app/descriptive-app
 
 :: make sure we update, in case it's not the first run
 wsl -u root -d Ubuntu -- git -C /usr/share/descriptive-app pull
 
 :: This is the main provisioner. We need root access.
 :: wsl -u root -d Ubuntu -- php -r "require '/home/'.strtolower('%username%').'/descriptive-app/installer/provision/ubuntu.php';" %username% wsl '%mypath%'
-wsl -u root -d Ubuntu -- php '/usr/share/descriptive-app/installer/provision/ubuntu.php'
+wsl -u root -d Ubuntu -- php '/home/descriptive-app/descriptive-app/installer/provision/ubuntu.php'
 
 start http://localhost:8080
 
