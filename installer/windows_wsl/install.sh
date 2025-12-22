72d200e0f29d6ae363740b310d54b4871fe6d63a
Alex Stanciu <ealexs@gmail.com>
2025-12-22 08:55:46 +0200

    update

--- installer/windows_wsl/install.sh
+++ installer/windows_wsl/install.sh
@@ -22,10 +22,10 @@ wsl --install -d Ubuntu
 	wsl -u root -d Ubuntu -- apt install -y git php
 
 # get the repo or update it
-	wsl -u root -d Ubuntu -- chown root:root /usr/share/descriptive-app -R
-	wsl -u root -d Ubuntu -- git clone https://github.com/alexstanciu-1/descriptive-programming.git /usr/share/descriptive-app
+	wsl -u root -d Ubuntu -- chown root:root '/usr/share/descriptive-app' -R
+	wsl -u root -d Ubuntu -- git clone 'https://github.com/alexstanciu-1/descriptive-programming.git' '/usr/share/descriptive-app'
 	# make sure we update, in case it's not the first run
-	wsl -u root -d Ubuntu -- git -C /usr/share/descriptive-app pull
+	wsl -u root -d Ubuntu -- git -C '/usr/share/descriptive-app' pull
 
 read -p "Press enter to continue"
 
