21c04f392b322c10f756abf3778d671b30b31827
Alex Stanciu <ealexs@gmail.com>
2025-12-22 10:03:59 +0200

    update

--- installer/windows_wsl/install.cmd
+++ installer/windows_wsl/install.cmd
@@ -5,7 +5,33 @@
 :: wslconfig /u Ubuntu
 
 cd /D "%~dp0"
-start /B /wait "" "%ProgramFiles%\Git\git-bash.exe" -c ./install.sh
-start /B /wait http://localhost:8080
+SET mypath="%~dp0"
+:: start /B /wait "" "%ProgramFiles%\Git\git-bash.exe" -c ./install.sh
 
-exit
+:: just for dev-mode for install scripts
+"%ProgramFiles%\Git\cmd\git" commit -am update
+"%ProgramFiles%\Git\cmd\git" pull
+"%ProgramFiles%\Git\cmd\git" push
+:: end
+
+wsl --install -d Ubuntu
+
+:: run a update if needed
+:: wsl --update
+
+:: minimal required to boot provision
+wsl -u root -d Ubuntu -- apt update
+wsl -u root -d Ubuntu -- apt install -y git php
+
+:: get the repo or update it
+wsl -u root -d Ubuntu -- chown root:root /usr/share/descriptive-app -R
+wsl -u root -d Ubuntu -- git clone https://github.com/alexstanciu-1/descriptive-programming.git /usr/share/descriptive-app
+:: make sure we update, in case it's not the first run
+wsl -u root -d Ubuntu -- git -C /usr/share/descriptive-app pull
+
+:: This is the main provisioner. We need root access.
+wsl -u root -d Ubuntu -- php '/usr/share/descriptive-app/installer/provision/ubuntu.php' wsl 'descriptive-app' '%mypath%'
+
+start http://localhost:8080
+
+pause
