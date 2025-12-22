95b61672196e87f0225405727a5a06c13051b2f5
Alex Stanciu <ealexs@gmail.com>
2025-12-22 09:57:13 +0200

    update

--- frontend/public_html/index.php
+++ frontend/public_html/index.php
@@ -13,7 +13,7 @@ $sync_path = "/mnt" . dirname(dirname($install_args->args[3]));
 
 var_dump('$sync_path', $sync_path);
 
-$cmd = "find -type f -not -path \"*/.git/*\" -not -path \"*/gens/*\" ".escapeshellarg($sync_path)."/";
+$cmd = "find -type f -not -path \"*/.git/*\" -not -path \"*/gens/*\" ".escapeshellarg($sync_path."/");
 
 var_dump($cmd);
 
